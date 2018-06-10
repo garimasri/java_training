878deade33575dbe3a7fea7b8edc4994c70cc06f
garimasri <38322498+garimasri@users.noreply.github.com>
2018-06-10 20:52:15 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,22 @@
+class af
+{
+public af()
+{
+System.out.println("java programming");
+}
+}
+class a extends af{
+public a()
+{
+super();
+System.out.println("best programming language");
+}
+}
+class basic
+{
+public static void main(String[] arg)
+{
+a b=new a();
+}
+}
+
